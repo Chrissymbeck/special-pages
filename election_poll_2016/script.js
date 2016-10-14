dbca06c75747917393fae75b16c681b8635bde60
Lucy Zhang <spothores9.lucy@gmail.com>
2016-10-13 23:01:45 -0400

    Working on bar chart

--- election_poll_2016/script.js
+++ election_poll_2016/script.js
@@ -2,7 +2,11 @@ var chart = c3.generate({
     bindto: '#barchart',
     data: {
         columns: [
-            ['Number of students who plan to vote for a specific candidate', 691, 61, 58, 34, 6]
+            ['Number of students who plan to vote for a specific candidate', 691, 61, 58, 34, 6],
+            ["Trinity",558,42,46,25,6],
+            ["Pratt",133,19,12,9,0],
+            ["Female",400,31,15,5,1],
+            ["Male",286,29,41,28,5]
         ],
         type: 'bar'
     },
