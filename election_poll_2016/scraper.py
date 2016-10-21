3c2d73a8208e9fa0f9da824c259ff94ff24a3331
Lucy Zhang <spothorse9.lucy@gmail.com>
2016-10-20 22:02:45 -0500

    Added voter registration map

--- election_poll_2016/scraper.py
+++ election_poll_2016/scraper.py
@@ -1,18 +1,27 @@
 import csv
 hillaryString = ''
 trumpString = ''
+voterRegistration={}
 with open('election_poll_anon.csv', 'rb') as csvfile:
 	spamreader = csv.reader(csvfile, delimiter=',')
 	for row in spamreader:
-
+		if not row[11]=='':
+			print row[11]
+			if row[11].replace(' ','') in voterRegistration:
+				print "in voterRegistration!"
+				voterRegistration[row[11].replace(' ','')]=voterRegistration[row[11].replace(' ','')]+1;
+			else:
+				voterRegistration[row[11].replace(' ','')]=1
+		'''
 		if not row[14]=='':
 			hillaryString = hillaryString +' '+ row[14].lower()
-		#print row[14] #hillary
 
 		if not row[15]=='':
 			trumpString = trumpString + ' '+row[15].lower().replace(',', '')
-		#print row[15]
+		'''
 
 
 
-print trumpString
+#line 11 is voter registration location
+
+print voterRegistration
