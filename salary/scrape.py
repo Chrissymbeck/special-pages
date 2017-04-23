4beb1bbe2d876f574cc34bff6207e332bcda8847
Lucy Zhang <spothorse9.lucy@gmail.com>
2017-04-22 23:54:48 -0400

    Format

--- salary/scrape.py
+++ salary/scrape.py
@@ -9,8 +9,14 @@ from bs4 import BeautifulSoup
 
 url = "http://api.glassdoor.com/api/api.htm?t.p="+partnerid+"&t.k="+key+"&userip=152.3.43.134&useragent=Mozilla&format=json&v=1&action=employers&q=duke"
 hdr = {'User-Agent': 'Mozilla/5.0'}
-req = urllib2.Request(url,headers=hdr)
-#print req
-response = urllib2.urlopen(req)
-print response.read()
-soup = BeautifulSoup(response)
+
+def main():
+	req = urllib2.Request(url,headers=hdr)
+	#print req
+	response = urllib2.urlopen(req)
+	print response.read()
+	soup = BeautifulSoup(response)
+
+
+if __name__ == '__main__':
+	main()
