738658585e6e9abbb39348028b3018c3ea4afe14
Lucy Zhang <spothorse9.lucy@gmail.com>
2017-04-22 23:49:57 -0400

    Reading glassdoor info

--- salary/scrape.py
+++ salary/scrape.py
@@ -1,2 +1,16 @@
+import requests
+import bs4
+
 partnerid = "142768"
-Key="iufju0hsnh1"
+key="iufju0hsnh1"
+
+import urllib2, sys
+from bs4 import BeautifulSoup
+
+url = "http://api.glassdoor.com/api/api.htm?t.p="+partnerid+"&t.k="+key+"&userip=152.3.43.134&useragent=Mozilla&format=json&v=1&action=employers&q=duke"
+hdr = {'User-Agent': 'Mozilla/5.0'}
+req = urllib2.Request(url,headers=hdr)
+#print req
+response = urllib2.urlopen(req)
+print response.read()
+soup = BeautifulSoup(response)
