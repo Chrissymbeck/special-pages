37554f8e543ad648c5768baa59724a2777d6fd72
Lucy Zhang <spothorse9.lucy@gmail.com>
2017-09-10 10:58:04 -0400

    Formatting

--- 2021-survey/script.js
+++ 2021-survey/script.js
@@ -32,6 +32,8 @@ var income = [
 ];
 var incomeData = ["Number of Students", 30, 37, 39, 60, 50, 27];
 
+var hiredAdm = [["Yes", 41], ["No", 207]];
+
 var colorPattern = ["#001A57"];
 
 function createPieChart(divElement, data, callback = null) {
@@ -74,6 +76,39 @@ function createBarChart(divElement, data) {
     });
 }
 
+function gaugeChart(divElement,data) {
+    var chart = c3.generate({
+        bindto: divElement,
+        data: {
+            columns: data,
+            type: 'gauge',
+            onclick: function(d, i) { console.log("onclick", d, i); },
+            onmouseover: function(d, i) { console.log("onmouseover", d, i); },
+            onmouseout: function(d, i) { console.log("onmouseout", d, i); }
+        },
+        gauge: {
+            //        label: {
+            //            format: function(value, ratio) {
+            //                return value;
+            //            },
+            //            show: false // to turn off the min/max labels.
+            //        },
+            //    min: 0, // 0 is default, //can handle negative min e.g. vacuum / voltage / current flow / rate of change
+            //    max: 100, // 100 is default
+            //    units: ' %',
+            //    width: 39 // for adjusting arc thickness
+        },
+        color: {
+            pattern: colorPattern, // the three color levels for the percentage values.
+            threshold: {
+                //            unit: 'value', // percentage is default
+                //            max: 200, // 100 is default
+                values: [30, 60, 90, 100]
+            }
+        },
+    });
+}
+
 function displayTestingScores(data, type) {
     $("#testType").html(type);
     $("#mean").html(data["mean"]);
@@ -90,4 +125,5 @@ createPieChart("#standardizedTesting", testing, function(data) {
     var test = data["id"];
     displayTestingScores(scores[test], test);
 });
+createPieChart("#hiredAdm", hiredAdm);
 createBarChart("#income", incomeData);
